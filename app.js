f84bb82ea82dcae339ab10fa9acccec3fbad09cf
Robert Loveless <robert@rwbl.co.uk>
2024-09-12 14:07:16 +0100

    add multer and file uploading

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var path = require('path');
+var multer = require('multer');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
@@ -16,5 +17,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now())
+  }
+});
+
+const upload = multer({ storage: storage });
+
+app.post('/upload', upload.single('file'), (req, res) => {
+  res.send('File uploaded successfully.');
+});
 
 module.exports = app;
