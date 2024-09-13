154f8140e0afd1ae9bb84ebd3bd1c2df0a25f439
Robert Loveless <robert@rwbl.co.uk>
2024-09-13 09:55:40 +0100

    point of demo

--- Dockerfile
+++ Dockerfile
@@ -5,4 +5,5 @@ RUN apk update && apk upgrade && rm -rf /var/cache/apk/*
 RUN apk add flux kubectl --repository=http://dl-cdn.alpinelinux.org/alpine/edge/community/
 RUN npm install express
 COPY . /app
+EXPOSE 3000
 CMD ["node","bin/www"]
--- app.js
+++ app.js
@@ -6,8 +6,14 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const {exec} = require("child_process");
 
 var app = express();
+
+var uploadedFile = '';
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -23,14 +29,95 @@ const storage = multer.diskStorage({
     cb(null, 'public/uploads')
   },
   filename: function (req, file, cb) {
-    cb(null, file.fieldname + '-' + Date.now())
+    cb(null, file.fieldname + '-' + Date.now() + '.zip')
   }
 });
 
 const upload = multer({ storage: storage });
 
-app.post('/upload', upload.single('file'), (req, res) => {
-  res.send('File uploaded successfully.');
+app.post('/upload', upload.single('file'),
+  async (req, res,next) => {
+  uploadedFile = req.file.path;
 });
+
+app.post('/run-flux', (req, res) => {
+  unzipUpload(uploadedFile);
+  console.log(req.body)
+  const formNamespace = req.body.namespace;
+  const formPath = req.body.path;
+  fluxCustomizeBuild(formNamespace, 'public/extracts/' + formPath);
+});
+
+app.get('/download', function (req, res) {
+  res.download('public/outputs/output.yaml');
+});
+
+
+function unzipUpload(uploadedFile) {
+  const {exec} = require("child_process");
+  exec("/usr/bin/unzip " + uploadedFile + " -d public/extracts/", (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+  });
+}
+
+function fluxCustomizeBuild(namespace, path) {
+  const {exec} = require("child_process");
+  exec("flux build kustomization " + namespace + " --path " + path + " >> public/outputs/output.yaml", (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+  });
+}
+
+function cleanupFiles() {
+  const {exec} = require("child_process");
+  exec("rm -rf public/extracts/*", (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+  });
+  exec("rm -rf public/uploads/*", (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+  });
+  exec("rm -rf public/outputs/*", (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+  });
+}
 
 module.exports = app;
